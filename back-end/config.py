e072ab02bf84f29ee281d7548126a5d3fc2d30d8
Filippo Principi <arkfil@gmail.com>
2024-03-26 17:27:01 +0100

    Updated database with 1 artwork updated upload_folder in config.py

--- back-end/config.py
+++ back-end/config.py
@@ -13,7 +13,7 @@ class Config(object):
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = None
-    UPLOAD_FOLDER = "static/assets/"
+    UPLOAD_FOLDER = "back-end/static/assets/"
 
 
 class ProductionConfig(Config):
